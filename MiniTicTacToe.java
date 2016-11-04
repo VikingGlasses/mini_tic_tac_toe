06f95c4e44e9fa1630211b21892e826647482464
Benjamin Rosman <rosman.benji@gmail.com>
2016-11-04 14:45:46 +0100

    Added bones to tic tac toe

--- MiniTicTacToe.java
+++ MiniTicTacToe.java
@@ -1,33 +1,75 @@
+import java.util.Scanner;
 
 public class MiniTicTacToe {
 	
-	private static char[][] board;
+	public static final char X = 'X';
+	public static final char O = 'O';
+	public static final char EMPTY = ' ';
+	
+	private char[][] board;
+	private Scanner keyboard;
 
 	public static void main(String[] args) {
+		MiniTicTacToe game = new MiniTicTacToe();
+		game.run();
+	}
+
+	private void run() {
+		
+		keyboard = new Scanner(System.in);
 		board = initiateBoard();
 		boolean playing = true;
+		int numberOfSquares = board.length * board[0].length;
+		String currentMove;
 		
 		// TODO print welcome message
 		System.out.println("Welcome to Tic Tac Toe");
 		// --loop-- 
 		while (playing) {
 			//   --take turn loop--
-			//     print board
-			//     print 'X' or 'O' turn to move
-			//     make a move
-			//     IF winning move
-			//       break turn loop
-			//     IF draw
-			//       print draw message
+			for (int turn = 0; turn < numberOfSquares; turn++) {
+				//     print board
+				printBoard();
+				//     print 'X' or 'O' turn to move
+				printTurn(turn);
+				//     make a move
+				currentMove = getMove();
+				//     IF winning move
+				if (winner(currentMove)) {
+					//       break turn loop
+					// print winner
+					break;
+				} else if (turn == numberOfSquares) {
+					// print draw message
+				}
+			}
 			//   --END turn loop--
 		}
 		// --END loop--
 		
-		
-
 	}
 
-	private static char[][] initiateBoard() {
+	private boolean winner(String currentMove) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private String getMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void printTurn(int turn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void printBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private char[][] initiateBoard() {
 		// TODO Auto-generated method stub
 		return null;
 	}
