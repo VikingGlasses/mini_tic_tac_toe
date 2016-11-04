8d5e5584f54c174b307b284abb172309bc030f90
Benjamin <rosman.benji@gmail.com>
2016-11-04 12:00:23 +0100

    Add MiniTicTacToe file

---
+++ MiniTicTacToe.java
@@ -0,0 +1,22 @@
+
+public class MiniTicTacToe {
+
+	public static void main(String[] args) {
+		// TODO print welcome message
+		// --loop-- 
+		//   --take turn loop--
+		//     print board
+		//     print 'X' or 'O' turn to move
+		//     make a move
+		//     IF winning move
+		//       break turn loop
+		//     IF draw
+		//       print draw message
+		//   --END turn loop--
+		// --END loop--
+		
+		
+
+	}
+
+}
