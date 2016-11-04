fea935d30123a76a90756e9995e1c3c92b9ea52c
Benjamin Rosman <rosman.benji@gmail.com>
2016-11-04 13:47:21 +0100

    started

--- MiniTicTacToe.java
+++ MiniTicTacToe.java
@@ -1,22 +1,35 @@
 
 public class MiniTicTacToe {
+	
+	private static char[][] board;
 
 	public static void main(String[] args) {
+		board = initiateBoard();
+		boolean playing = true;
+		
 		// TODO print welcome message
+		System.out.println("Welcome to Tic Tac Toe");
 		// --loop-- 
-		//   --take turn loop--
-		//     print board
-		//     print 'X' or 'O' turn to move
-		//     make a move
-		//     IF winning move
-		//       break turn loop
-		//     IF draw
-		//       print draw message
-		//   --END turn loop--
+		while (playing) {
+			//   --take turn loop--
+			//     print board
+			//     print 'X' or 'O' turn to move
+			//     make a move
+			//     IF winning move
+			//       break turn loop
+			//     IF draw
+			//       print draw message
+			//   --END turn loop--
+		}
 		// --END loop--
 		
 		
 
+	}
+
+	private static char[][] initiateBoard() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
